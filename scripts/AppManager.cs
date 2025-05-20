ad15ea66046429e253d5dc78b3c2124581afe98d
Sloeffler123 <samloeffler123@gmail.com>
2025-05-20 09:47:10 -0500

    added table to node

---
+++ scripts/AppManager.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+public partial class AppManager : Control
+{
+    [Export]
+    public PackedScene TransactionList;
+    Budget currentBudget;
+    public override void _Ready()
+    {
+        currentBudget = new Budget()
+        {
+            Name = "current budget",
+            Expenses = new List<Transaction>(),
+            Income = new List<Transaction>()
+        };
+        currentBudget.Expenses.Add(new Transaction()
+        {
+            Name = "test",
+            Date = DateTime.Now,
+            Amount = 50,
+            Type = "home"
+        });
+        currentBudget.Expenses.Add(new Transaction()
+        {
+            Name = "test2",
+            Date = DateTime.Now,
+            Amount = 501,
+            Type = "home"
+        });
+        currentBudget.Income.Add(new Transaction()
+        {
+            Name = "paycheck 1",
+            Date = DateTime.Now,
+            Amount = 2000,
+            Type = "home"
+        });
+        VBoxContainer container = GetNode<VBoxContainer>("ScrollContainer/TransactionList");
+        float totalExpense = 0;
+        foreach (var value in currentBudget.Expenses)
+        {
+            Node tableRow = TransactionList.Instantiate();
+            tableRow.GetNode<RichTextLabel>("Date").Text = value.Date.ToString();
+            tableRow.GetNode<RichTextLabel>("Name").Text = value.Name;
+            tableRow.GetNode<RichTextLabel>("Amount").Text = value.Amount.ToString();
+            tableRow.GetNode<RichTextLabel>("Type").Text = value.Type;
+            container.AddChild(tableRow);
+            totalExpense += value.Amount;
+        }
+        GetNode<RichTextLabel>("TotalInE/ExpenseAmount").Text = totalExpense.ToString();
+        float totalIncome = 0;
+        foreach (var value in currentBudget.Income)
+        {
+            Node tableRow = TransactionList.Instantiate();
+            tableRow.GetNode<RichTextLabel>("Date").Text = value.Date.ToString();
+            tableRow.GetNode<RichTextLabel>("Name").Text = value.Name;
+            tableRow.GetNode<RichTextLabel>("Amount").Text = value.Amount.ToString();
+            tableRow.GetNode<RichTextLabel>("Type").Text = value.Type;
+            container.AddChild(tableRow);
+            totalIncome += value.Amount;
+        }
+        GetNode<RichTextLabel>("TotalInE/IncomeAmount").Text = totalIncome.ToString();
+    }
+}
---
+++ scripts/Transaction.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Transaction
+{
+    public string Name;
+    public float Amount;
+    public DateTime Date;
+    public string Type;
+}
